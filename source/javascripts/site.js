0f5e56a82c423a2977cfab43f23bf31644297115
Andrew McCarthy <hi@andrevv.com>
2017-07-24 12:10:20 +0200

    content

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -5,50 +5,74 @@ var scrollTop = 0;
 window.addEventListener('load', loop);
 window.addEventListener('scroll', onScroll);
 var el;
+var mondo;
 var img;
 var img2;
 var img3;
+var text;
+var quote;
+var list;
 var per = 0;
-var userScroll = false;
+var velocity = 0;
+var looping = false;
 
 function loop() {
   el = document.getElementById('scroller');
+  mondo = document.getElementById('mondo_img');
   img = document.querySelector('.mondo_1');
   img2 = document.querySelector('.mondo_2');
   img3 = document.querySelector('.mondo_3');
   img4 = document.querySelector('.mondo_4');
+  text = document.querySelector('#content');
+  quote = document.getElementById('quote');
+  list = document.getElementById('list');
   window.requestAnimationFrame(function() {
 
-    if(!userScroll)
-      window.scrollTo(0, scrollTop + 1);
+    velocity *= 0.8;
 
     per = (scrollTop / (el.offsetHeight - window.innerHeight));
 
     if(per >= 0.3526075141267308) {
       //console.log(per);
       var reset = (el.offsetHeight - window.innerHeight) * (per * 0.4801516912);
-      window.scrollTo(0, reset);
-      //scrollTop = window.scrollY;
+      //looping = true;
+      //window.scrollTo(0, reset);
+      
     }
 
-    var minus = 2400 * per;
-    var rotate = 70 * per;
-    var y = -100 * per;
+    var minus = 550 * per;
+    var rotate = 280 * per;
+    var y = -8 * per;
 
+    mondo.style.transform = "rotateX(-70deg) rotateZ("+ (1 - velocity) +"deg) rotateY(0deg)";
     img.style.transform = "translate3d(-50%,"+(-y + 22)+"vh,"+(280 - minus)+"vh) scale3d(8,1,1) rotateZ(90deg) rotateY(-90deg)";
-    img2.style.transform = "translate3d(-50%,"+(-y + 2)+"vh,"+(720 - minus)+"vh) scale3d(8,1,1) rotateZ(90deg) rotateY(-90deg)";
+    //img2.style.transform = "translate3d(-50%,"+(-y + 2)+"vh,"+(720 - minus)+"vh) scale3d(8,1,1) rotateZ(90deg) rotateY(-90deg)";
+    text.style.transform = "translate3d(0,"+((y * 500))+"px,0)";
 
-    
+    var textRect = quote.getBoundingClientRect();
+    var listRect = list.getBoundingClientRect();
+    if(listRect.bottom < -window.innerHeight + 50 ) {
+      var offset = listRect.bottom + window.innerHeight - 50;
+      quote.style.transform = "translate3d(0,"+(-offset)+"px,0)";
+    }
 
     loop();
 
-    userScroll = false;
+    looping = false;
+
   });
 }
 
 function onScroll() {
   window.requestAnimationFrame(function() {
-    userScroll = true;
     scrollTop = window.scrollY;
   });
 }
+
+var handleScroll = function(evt){
+  if (!evt) evt = event;
+  var direction = (evt.detail<0 || evt.wheelDelta>0) ? 1 : -1;
+  velocity += evt.wheelDelta * 0.00025;
+};
+document.addEventListener('DOMMouseScroll',handleScroll,false); // for Firefox
+document.addEventListener('mousewheel',    handleScroll,false);
