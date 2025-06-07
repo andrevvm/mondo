aebbc9edc8787ce2a8b22d90bc396e18c10bd5c7
Andrew McCarthy <hi@andrevv.com>
2025-06-07 16:34:49 -0700

    updates

--- config.rb
+++ config.rb
@@ -13,6 +13,10 @@ page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
 
+# Deploy configuration removed due to incompatibility with Middleman 4.2+
+# Use the deployment script (./deploy.sh) or GitHub Actions instead
+# See deployment instructions in the README
+
 # With alternative layout
 # page '/path/to/file.html', layout: 'other_layout'
 
