4d9815b634e267bc79957349d73bea1754dceb44
Andrew McCarthy <hi@andrevv.com>
2025-06-08 11:04:28 -0700

    updates

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,9 +1,4 @@
-// This is where it all goes :)
-
-var scrollTop = 0
-
 window.addEventListener('load', loop)
-window.addEventListener('scroll', onScroll)
 var el
 var mondo
 var img
@@ -14,9 +9,9 @@ var quote
 var list
 var per = 0
 var velocity = 0
-var looping = false
 var offset = 0
 var wheel = 0
+var y = 0
 
 function loop() {
   el = document.body
@@ -26,23 +21,27 @@ function loop() {
   img3 = document.querySelector('.mondo_3')
   img4 = document.querySelector('.mondo_4')
   window.requestAnimationFrame(function () {
-    offset -= 0.05
+    if (
+      window.scrollY >=
+      window.document.body.scrollHeight - window.innerHeight
+    ) {
+      window.scrollTo({
+        top: 0,
+        behavior: 'instant',
+      })
+    }
+
+    if (offset > -0.004) offset = -0.004
+
+    offset -= 0.0005
     velocity *= 0.8
 
-    // per = wheel
+    if (y < -2.2) {
+      offset = -0.555
+      wheel = 0
+    }
 
-    // if (per >= 0.5526075141267308) {
-    //   //console.log(per);
-    //   var reset = (el.offsetHeight - window.innerHeight) * (per * 0.4801516912)
-    //   looping = true
-    //   window.scrollTo(0, reset)
-    // }
-
-    var minus = 550 * per
-    var rotate = 280 * per
-    var y = -200 * per + offset + wheel
-
-    console.log(y)
+    y = offset + wheel
 
     mondo.style.transform =
       'rotateX(-70deg) rotateZ(' + (1 - velocity) + 'deg) rotateY(0deg)'
@@ -50,33 +49,19 @@ function loop() {
       'translate3d(-50%,' +
       (-y + 17) +
       'vh,' +
-      (280 - minus) +
+      (280 + y * 280) +
       'vh) scale3d(8,1,1) rotateZ(90deg) rotateY(-90deg)'
-    img2.style.transform =
-      'translate3d(-50%,' +
-      (-y - 220) +
-      'vh,' +
-      (280 - minus) +
-      'vh) scale3d(8,1,1) rotateZ(90deg) rotateY(-90deg)'
-    // text.style.transform = 'translate3d(0,' + y * 500 + 'px,0)'
 
     loop()
-
-    looping = false
-  })
-}
-
-function onScroll() {
-  window.requestAnimationFrame(function () {
-    scrollTop = window.scrollY
   })
 }
 
 var handleScroll = function (evt) {
-  if (!evt) evt = event
-  var direction = evt.detail < 0 || evt.wheelDelta > 0 ? 1 : -1
+  if (!evt.wheelDelta) return
+  if (y > 0) return
+
   velocity += evt.wheelDelta * 0.00025
-  wheel += evt.wheelDelta * 0.01
+  wheel += evt.wheelDelta * 0.0001
 }
 document.addEventListener('DOMMouseScroll', handleScroll, false) // for Firefox
 document.addEventListener('mousewheel', handleScroll, false)
